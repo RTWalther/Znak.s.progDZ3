309b90fd006fbd5e84f3718870cfe5cab5a3f120
RTWalther <mr.tohtaev1998@mail.ru>
2022-10-27 01:06:08 +0500

    NEW uppdate

--- TASK23/Program.cs
+++ TASK23/Program.cs
@@ -2,15 +2,21 @@
 // Напишите программу, которая 
 // 1. принимает на вход число (N) и
 // 2. выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
 
 void Table(int n)
 {
     if (n > 0)
     {
-        int index = 3;
+        int index = 1;
         while (index <= n)
         {
-            Console.WriteLine($"{index,4} {index*index*index,4}");
+            Console.WriteLine($"{index,1}|{index*index*index,1}");
             index++;
         }
     }
